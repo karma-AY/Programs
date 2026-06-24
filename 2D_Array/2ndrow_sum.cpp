c2762abdf25ea06212c2cdd3b0da64e98c2fc779
AtharvaY <atharvarajeshyeole@gmail.com>
2026-06-25 02:08:38 +0530

    made spiral using dynamic array and started vectors in cpp

--- 2D_Array/2ndrow_sum.cpp
+++ 2D_Array/2ndrow_sum.cpp
@@ -1,29 +1,28 @@
 /*Print out the sum of the numbers in the second row of the“nums” array*/
+#include <_stdio.h>
 #include <iostream>
 using namespace std;
 
-int second_row_sum(int mat[][100],int n,int m);
+int second_row_sum(int mat[][100], int n, int m);
 int main() {
-    int row,col;
-    cout << "Enter row and col: ";
-    cin >> row >> col;
-    int mat[100][100];
-    cout << "Enter the elements of matrix:\n";
-    for(int i=0; i<row; i++)
-    {
-        for(int j=0; j<col; j++)
-        {
-            cin >> mat[i][j];
-        }
+  int row, col;
+  cout << "Enter row and col: ";
+  cin >> row >> col;
+  int mat[100][100];
+  cout << "Enter the elements of matrix:\n";
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < col; j++) {
+      cin >> mat[i][j];
     }
-    int ans = second_row_sum(mat, row, col);
-    cout << "The sum of 2nd row is:" << ans << endl;
+  }
+  int ans = second_row_sum(mat, row, col);
+  cout << "The sum of 2nd row is:" << ans << endl;
   return 0;
 }
-int second_row_sum(int mat[][100],int n,int m){
-    int sum = 0;
-    for(int i = 0;i<m;i++){
-       sum+= *(*(mat+1)+i);
-    }
-    return sum;
+int second_row_sum(int mat[][100], int n, int m) {
+  int sum = 0;
+  for (int i = 0; i < m; i++) {
+    sum += *(*(mat + 1) + i);
+  }
+  return sum;
 }
--- 2D_Array/spiral.cpp
+++ 2D_Array/spiral.cpp
@@ -1,49 +1,44 @@
 #include <iostream>
 using namespace std;
 
-void spiralm(int mat[][4], int n, int m)
+void spiralm(int *mat, int n, int m)
 {
-    int sr = 0, sc =0, er = n-1, ec = m-1;
-
-    while(sr<=er&&sc<=ec)
+    int sr = 0, sc = 0, er = n-1, ec = m-1;
+    while(sr <= er && sc <= ec)
     {
+        for(int i = sc; i <= ec; i++)
+            cout << mat[sr*m + i] << " ";
 
-        //top
-        for(int i = sc; i<=ec; i++)
-        cout << mat[sr][i] << " ";
+        for(int j = sr+1; j <= er; j++)
+            cout << mat[j*m + ec] << " ";
 
-        //right
-        for(int j=sr+1; j<=er; j++)
-        cout << mat[j][ec] << " ";
+        if(sr != er)
+            for(int k = ec-1; k >= sc; k--)
+                cout << mat[er*m + k] << " ";
 
-        //bottom 
-        for(int k =ec-1; k>=sc; k--)
-        cout << mat[er][k] << " ";
+        if(sc != ec)
+            for(int l = er-1; l >= sr+1; l--)
+                cout << mat[l*m + sc] << " ";
 
-        //left
-        for(int l=er-1; l>=sr+1; l--)
-        cout << mat[l][sc] << " ";
-        sr++;
-        sc++;
-        er--;
-        ec--;
-        
+        sr++; sc++; er--; ec--;
     }
 }
-int main() 
+
+int main()
 {
-    int row,col;
+    int row, col;
     cout << "Enter row and col: ";
     cin >> row >> col;
-    int mat[row][col];
+
+    int *mat = new int[row * col];
+
     cout << "Enter the elements of matrix:\n";
-    for(int i=0; i<row; i++)
-    {
-        for(int j=0; j<col; j++)
-        {
-            cin >> mat[i][j];
-        }
-    }
-    spiralm(mat,row,col);
-  return 0;
+    for(int i = 0; i < row; i++)
+        for(int j = 0; j < col; j++)
+            cin >> mat[i*col + j];
+
+    spiralm(mat, row, col);
+
+    delete[] mat;
+    return 0;
 }
